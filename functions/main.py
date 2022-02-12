b01b82bedcb321aa43cd6ec884ab7f3d8028af9d
BrayanCalderon <bjcalderona@unal.edu.co>
2022-02-11 19:32:51 -0500

    Sustentacion

--- functions/main.py
+++ functions/main.py
@@ -2,6 +2,7 @@ import cv2
 from preproces import *
 from red import *
 import tensorflow as tf
+import time
 
 
 cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
@@ -9,50 +10,52 @@ cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
 #Check de si funciona la camara web
 if not cap.isOpened():
     raise IOError("No se pudo abrir la webcam")
+model = cargar_red()
 
 while True:
     ret, frame = cap.read()
+    
     frame_contorno,rects  = contornos(frame)
+    imgs = recortes(frame,rects)
+    #
+    # print(len(imgs))
+
 
     font = cv2.FONT_HERSHEY_SIMPLEX
     org = (50, 50)
-    fontScale = 1
-    color = (255, 0, 0)
+    fontScale = 0.7
+    color = (0, 0, 0)
     thickness = 2
-    image = cv2.putText(frame, 'MateoCareverga', org, font, 
-                   fontScale, color, thickness, cv2.LINE_AA)
     
-    #new_model = keras.models.load_model('modelo.h5')
+    switches = {'Switches 1' : 0,'Switches 2' : 0,'Switches 3' : 0,'Switches 4' : 0,'Switches 5' : 0,}
 
 
+    def escribirpantalla(k, value, i):
+        org = (50,50+(i*30))
+        image = cv2.putText(frame, k+"-->"+str(value), org, font, 
+                   fontScale, color, thickness, cv2.LINE_AA)
 
-    # Load the TFLite model and allocate tensors.
-    interpreter = tf.lite.Interpreter(model_path="model.tflite")
-    interpreter.allocate_tensors()
-
-    # Get input and output tensors.
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-
-    # Test the model on random input data.
-    input_shape = input_details[0]['shape']
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-
-    interpreter.invoke()
-
-    # The function `get_tensor()` returns a copy of the tensor data.
-    # Use `tensor()` in order to get a pointer to the tensor.
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    print(output_data)
-
-
-
-
-    #imgs = recortes(frame_contorno,rects)
-
-    #for image in imgs:
-    #    print(model.predict(image))
+    
+    for img in imgs:
+        img = img.reshape(1,100,100,1)
+        result = prediccion(img,model)
+        #print(result.shape)
+        #print(type(result))
+        if result[0,0] == np.amax(result[0]):
+            switches["Switches 1"] = switches.get("Switches 1",0) +1
+        elif result[0,1] == np.amax(result[0]):
+            switches["Switches 2"] = switches.get("Switches 2",0) +1
+        elif result[0,2] == np.amax(result[0]):
+            switches["Switches 3"] = switches.get("Switches 3",0) +1
+        elif result[0,3] == np.amax(result[0]):
+            switches["Switches 4"] = switches.get("Switches 4",0) +1
+        elif result[0,4] == np.amax(result[0]):
+            switches["Switches 5"] = switches.get("Switches 5",0) +1
+    i = 0
+    for k in switches:
+        print(i,k, switches[k])
+        escribirpantalla(k,switches[k],i)
+        i+=1
 
     try:
         cv2.imshow('Input', frame_contorno)
@@ -62,6 +65,7 @@ while True:
     c = cv2.waitKey(1)
     if c == 27:
         break
+    #time.sleep(1) # Sleep for 3 seconds
 
 cap.release()
 cv2.destroyAllWindows()
--- functions/red.py
+++ functions/red.py
@@ -1,5 +1,10 @@
 import tensorflow as tf
-from tensorflow import keras
 
-def red_neuronal():
-    return keras.models.load_model('modelo.h5')
+def cargar_red():
+    model = tf.keras.models.load_model('C:/Users/Brayan_Calderon/Documents/Github/clasificadorTIA/functions/modelo.h5')
+    model.summary()
+    return model
+
+def prediccion(img,model):
+    prediccion = model.predict(img)
+    return prediccion
